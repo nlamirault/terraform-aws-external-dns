6a167fe893771f6fb23998ce0fd19746eabfcc2e
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-02-18 08:22:28 +0100

    Update: refactoring Terraform code using Tflint
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -12,18 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-data "aws_secretsmanager_secret" "oidc_url" {
-  name = format("%s_k8s_oidc_url", replace(var.cluster_name, "-", "_"))
-}
-
-data "aws_secretsmanager_secret_version" "oidc_url" {
-  secret_id = data.aws_secretsmanager_secret.oidc_url.id
-}
-
-data "aws_secretsmanager_secret" "oidc_arn" {
-  name = format("%s_k8s_oidc_arn", replace(var.cluster_name, "-", "_"))
-}
-
-data "aws_secretsmanager_secret_version" "oidc_arn" {
-  secret_id = data.aws_secretsmanager_secret.oidc_arn.id
+output "role_arn" {
+  value       = element(aws_iam_role.external_dns.*.arn, 0)
+  description = "Amazon Resource Name"
 }
